728b09ec8faa133988b4e69e8994f7b260590a04
0x4B_0x49_0x52 <devzetsubou@gmail.com>
2021-02-02 16:19:40 +0300

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6019515b10485c001e6569eb",
+				"6019515b10485c001e6569ed"
+			]
+		},
+		"6019515b10485c001e6569eb": {
+			"id": "6019515b10485c001e6569eb",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6019515b10485c001e6569ed": {
+			"id": "6019515b10485c001e6569ed",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
